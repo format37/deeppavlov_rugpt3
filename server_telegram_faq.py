a0955888054da6cae7d3f4348960e57fda0f8c32
format37 <format37@gmail.com>
2020-10-26 23:50:38 +0300

    modified:   server_telegram_faq.py

--- server_telegram_faq.py
+++ server_telegram_faq.py
@@ -7,17 +7,16 @@ WEBHOOK_LISTEN = '0.0.0.0'  # In some VPS you may need to put here the IP addr
 async def call_test(request):
 	return web.Response(text='ok',content_type="text/html")
 
+async def call_talk(request):
+	question = request.rel_url.query['question']	
+	return web.Response(text=question,content_type="text/html")
+
 app = web.Application()
 app.router.add_route('GET', '/test', call_test)
+app.router.add_route('GET', '/question', call_talk)
 
-# Build ssl context
-#context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
-#context.load_cert_chain(WEBHOOK_SSL_CERT, WEBHOOK_SSL_PRIV)
-
-# Start aiohttp server
 web.run_app(
     app,
     host=WEBHOOK_LISTEN,
     port=WEBHOOK_PORT,
-    #ssl_context=context,
 )
